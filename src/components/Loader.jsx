6f7063ad0138c3fd6260a7898a5b27d5794f33bf
Yuvraj <itsyuvi@outlook.com>
2024-01-24 07:38:24 +0530

    added loader

---
+++ src/components/Loader.jsx
@@ -0,0 +1,7 @@
+const Loader = () => {
+  return (
+    <div className="w-12 h-12 rounded-full animate-spin border-4 border-solid border-slate-400 border-t-transparent"></div>
+  );
+};
+
+export default Loader;
--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import ProductCard from "../components/ProductCard";
+import Loader from "../components/Loader";
 
 const Product = () => {
   const [products, setProducts] = useState([]);
+  const [isLoading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -12,6 +14,8 @@ const Product = () => {
         setProducts(data);
       } catch (error) {
         console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
       }
     };
 
@@ -19,14 +23,22 @@ const Product = () => {
   }, []);
 
   return (
-    <div className="container mx-auto p-5">
-      <h1 className="text-2xl font-bold mb-4">Product List</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {products.map((product, index) => {
-          return <ProductCard key={index} product={product} />;
-        })}
-      </div>
-    </div>
+    <>
+      {isLoading ? (
+        <div className="flex justify-center items-center min-h-[50vh]">
+          <Loader />
+        </div>
+      ) : (
+        <div className="container mx-auto p-5">
+          <h1 className="text-2xl font-bold mb-4">Product List</h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
+            {products.map((product, index) => {
+              return <ProductCard key={index} product={product} />;
+            })}
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
